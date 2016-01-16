64eea55ac3293969f892ed556fc89fd8fd21435c
John McIntosh II <jmci10@hotmail.com>
2016-01-16 08:19:28 +0000

    complete exercises 1 and 2

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,5 +5,12 @@ class ApplicationController < ActionController::Base
 
   def hello
     render text: ('a'..'z').to_a.shuffle[0..7].join + " Hello, World!"
+    # exercise 1: render hola, mundo! instead of Hello, World!
+    # render text: "hola, mundo!"
+  end
+  
+  # exercise 2: define goodbye action, render goodbye, world!, change route to point to application#goodbye
+  def goodbye
+    render text: "goodbye, world!"
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
   
   root "application#hello"
-  
+  # root "application#goodbye"
 end
